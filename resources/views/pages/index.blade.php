6c10af8967bb813bba621c1bc76450cc91aa0c46
Daniel-Sogbey <mathematics06physics@gmail.com>
2022-02-03 02:20:36 +0000

    added services,about, index  route and controller

--- resources/views/pages/index.blade.php
+++ resources/views/pages/index.blade.php
@@ -8,8 +8,8 @@
 </head>
 
 <body>
-    <h1>About</h1>
-    <p>This is the about page.</p>
+    <h1>Welcome to Laravel</h1>
+    <p>This is the Laravel application from the 'Laravel From Scratch Youtube Series".</p>
 </body>
 
 </html>
--- routes/web.php
+++ routes/web.php
@@ -13,17 +13,19 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
-
 /*
 Route::get('/hello', function () {
     // return view('welcome');
     return "<h1>Hello World</h1>";
 });
+
+Route::get("/users/{id}/{name}", function($id, $name) {
+    return "This is user #".$name." with an id of ".$id;
+});
 */
 
-Route::get("/about", function() {
-    return view("pages.about");
-});
+Route::get('/', 'PagesController@index');
+
+Route::get("/about", 'PagesController@about');
+
+Route::get("/services","PagesController@services");
