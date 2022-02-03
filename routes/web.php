89e611b2d01884ee76c5e46653924c165f66a3a3
Daniel-Sogbey <mathematics06physics@gmail.com>
2022-02-03 01:18:11 +0000

    created new route

--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,8 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/hello', function () {
+    // return view('welcome');
+    return "<h1>Hello World</h1>";
+});
