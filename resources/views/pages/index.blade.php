9887b95d198d67bebe3fcede724c1e19094cf265
Daniel-Sogbey <mathematics06physics@gmail.com>
2022-02-03 02:27:05 +0000

    added layout

--- resources/views/pages/index.blade.php
+++ resources/views/pages/index.blade.php
@@ -1,15 +1,5 @@
-<!DOCTYPE html>
-<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
-
-<head>
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-    <title>{{config('app.name')}}</title>
-</head>
-
-<body>
+@extends("layouts.app")
+@section("content");
     <h1>Welcome to Laravel</h1>
     <p>This is the Laravel application from the 'Laravel From Scratch Youtube Series".</p>
-</body>
-
-</html>
+@endsection
