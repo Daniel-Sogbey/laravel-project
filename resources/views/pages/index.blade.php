d6770fb98641a7bb784766a98728d6bd657f25f9
Daniel-Sogbey <mathematics06physics@gmail.com>
2022-02-03 14:01:58 +0000

    update-- runned npm install

--- resources/views/pages/index.blade.php
+++ resources/views/pages/index.blade.php
@@ -1,5 +1,5 @@
 @extends("layouts.app")
-@section("content");
-    <h1>Welcome to Laravel</h1>
+@section("content")
+    <h1>{{$title}}</h1>
     <p>This is the Laravel application from the 'Laravel From Scratch Youtube Series".</p>
 @endsection
