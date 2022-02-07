140a934b3b3168cdc198df5ac2ab4d22cee4f54b
Daniel-Sogbey <mathematics06physics@gmail.com>
2022-02-07 00:44:23 +0000

    added access controll

--- resources/views/posts/show.blade.php
+++ resources/views/posts/show.blade.php
@@ -9,9 +9,13 @@
 <hr>
 <small>Written on {{$post->created_at}} by {{$post->user->name}}</small>
 <hr>
+@if(!Auth::guest())
+@if(Auth::user()->id == $post->user_id)
 <a href="/posts/{{$post->id}}/edit" class="btn btn-default my-3">Edit</a>
 {!!Form::open(["action"=>["PostsController@destroy",$post->id],"method"=>"POST","class"=>"pull-right"])!!}
 {{Form::hidden("_method","DELETE")}}
 {{Form::submit('Delete',['class'=>"btn btn-danger"])}}
 {!!Form::close()!!}
+@endif
+@endif
 @endsection
